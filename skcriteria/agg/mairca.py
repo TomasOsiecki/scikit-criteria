d2f54708d8db3a7e10fc5739221fc4ec0ea1d8ce
gastonstrizzolo <gastonstrizzolo@gmail.com>
2025-06-14 18:14:34 -0300

    Step 4 in progress

--- skcriteria/agg/mairca.py
+++ skcriteria/agg/mairca.py
@@ -13,6 +13,35 @@ with hidden():
 # MAIRCA
 # =============================================================================
 
+# decision_matrix = np.array(
+#     [
+#         [3.828,	5.000,	3.720,	2.723,	1.000,	4.255],
+#         [4.675,	5.000,	3.000,	3.452,	1.000,	2.587],
+#         [4.515,	4.836,	3.289,	3.491,	1.000,	4.069],
+#         [4.421,	5.000,	3.555,	2.839,	1.000,	4.397],
+#         [4.717,	5.000,	3.430,	4.401,	1.000,	1.000],
+#         [4.695,	5.000,	3.925,	3.847,	1.000,	1.000],
+#         [4.688,	5.000,	2.000,	4.99,	1.000,	1.000],
+#         [4.688,	5.000,	3.971,	4.001,	1.000,	1.000],
+#     ]
+# )
+
+# n columnas, m filas
+# caso de test: la suma de los p_ai's debe ser 1
+
+# si hay una columna con todos 1, se hace:
+# decision_matrix = np.delete(decision_matrix, ...)
+# weights = np.array([0.2016, 0.2304, 0.2232, 0.1912, 0.1536])
+
+def calculate_real_evaluation(matrix, a, b):
+    """
+    (x_ij - a) / (b - a)
+    in the maximization case, a is the minimum value of the column, b is the maximum value of the column
+    in the minimization case, b is the maximum value of the column, a is the minimum value of the column
+    """
+    # TODO: intentar con for's
+    pass
+
 def mairca(matrix, objectives, weights):
     """ 
     Execute MAIRCA without any validation.
@@ -25,13 +54,20 @@ def mairca(matrix, objectives, weights):
 
     # Step 2
     # Calculate  P_ai
-    # P_ai = 1 / len(X)
+    # m = len(X)
+    # P_ai = 1 / m
 
     # Step 3
     # Define the theoretical ranking matrix (Tp)
+    # T_p = np.tile(weights * P_ai, (m, 1))
 
     # Step 4
     # Define the real rating matrix (Tr)
+    # x_i_min = decision_matrix.min(axis=0)
+    # x_i_max = decision_matrix.max(axis=0)
+    # mask = objectives == 1
+    # real_value = calculate_real_evaluation(X)
+    # T_r = T_p * real_value
 
     # Step 5
     # Calculate Total Gap Matrix
